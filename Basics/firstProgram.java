21688a74c79aa40f2465e918f9d82ea493f322ea
SAGAR SATYARTH <sagarsatyarthh@gmail.com>
2025-07-27 20:51:57 +0530

    firstPromgram in java

---
+++ Basics/firstProgram.java
@@ -0,0 +1,5 @@
+ public class firstProgram{ // class name as the file name
+    public static void main(String[] args) { // main method
+        System.out.println("Hello World !"); // for printing on console.
+    }
+ }
