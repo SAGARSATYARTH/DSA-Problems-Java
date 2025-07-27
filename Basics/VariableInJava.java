b7115ee3d5a3a0e4c23ee35b3da8fc9b19b46fc1
SAGAR SATYARTH <sagarsatyarthh@gmail.com>
2025-07-27 21:04:14 +0530

    Variable in Java

---
+++ Basics/VariableInJava.java
@@ -0,0 +1,13 @@
+public class VariableInJava {
+    public static void main(String[] args) {
+        int a=10; // a is integer type variable ,storing 10
+        float b=20.5f; // b is float type variable ,storing decimal type value 20.5
+        boolean aIsInt=true; // aIsInt is boolean type which store true/false.
+
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
+        float sum=a+b; // float can store integer type value but a int can not store float type value.
+        System.out.println("Sum : "+ sum);
+    }
+    
+}
