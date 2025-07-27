bca2e9a073fb6c641d228948aaf8f0399ebf5a79
SAGAR SATYARTH <sagarsatyarthh@gmail.com>
2025-07-27 21:10:16 +0530

    You can vote or not

---
+++ Basics/IfElse.java
@@ -0,0 +1,15 @@
+//simple if-else program for checking who is eligible for vote..
+
+public class IfElse {
+    public static void main(String[] args) {
+        int age =18;
+
+        if(age>=18){ // for checking condition, if the condition is true then if statement will print.
+            System.out.println("You are eligible to Vote");
+        }
+        else{ // if the condition is false then the else statement will print...
+            System.out.println("You Can not vote");
+        }
+    }
+    
+}
