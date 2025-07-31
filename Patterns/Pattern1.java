a9096a880d04e53408fc220d180fb728dd74419e
SAGAR SATYARTH <sagarsatyarthh@gmail.com>
2025-07-31 12:44:00 +0530

    changed the limit of array

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -11,11 +11,12 @@ public class Pattern1 {
         System.out.print("Enter n : ");
         int n=sc.nextInt();
 
-        for(int i=1;i<=5;i++){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
+        sc.close();
     }
 }
