c266e09b852537c50ee891eca42efde43301baf8
SAGAR SATYARTH <sagarsatyarthh@gmail.com>
2025-08-31 17:41:38 +0530

    Initial commit

---
+++ Recursion/FirstIndexOfOccurrence.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class FirstIndexOfOccurrence {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+
+        System.out.print("Enter index : ");
+        int n=scn.nextInt();
+
+        int arr[]=new int[n];
+        System.out.println("Enter elements : ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+        System.out.print("Enter k : ");
+        int k=scn.nextInt();
+        System.out.println("at index : "+firstOccurrence(arr, 0 ,8));
+
+        scn.close();
+    }
+
+    public static int firstOccurrence(int arr[],int idx,int k){
+        if(arr.length==idx){
+            return -1;
+        }
+        if(arr[idx]==k){
+            return idx;
+        }
+        return firstOccurrence(arr, idx+1, k);
+    }
+    
+}
