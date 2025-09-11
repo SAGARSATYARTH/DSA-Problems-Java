6f5fc540e1c24c29ae9c78b6214c750ac016d809
SAGAR SATYARTH <sagarsatyarthh@gmail.com>
2025-09-11 23:56:10 +0530

    Initial commit

---
+++ Time and Space/BubbleSort.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+
+        System.out.print("Enter no. elements in array : ");
+        int n=scn.nextInt();
+
+        int arr[]=new int[n];
+
+        System.out.println("Enter "+n+" of elements : ");
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+
+        System.out.println("Elements of array : ");
+        display(arr);
+
+        bubbleSort(arr);
+
+        display(arr);
+
+
+    }
+
+    public static void bubbleSort(int arr[]){
+        for(int i=1;i<=arr.length-1;i++){
+            for(int j=0;j<arr.length-i;j++){
+                if(isSmaller(arr,j+1,j)){
+                    swap(arr,j+1,j);
+                }
+            }
+        }
+    }
+
+    public static void swap(int arr[],int i,int j){
+        System.out.println("swapping "+arr[i]+" and "+arr[j]);
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    public static boolean isSmaller(int arr[],int i,int j){
+        System.out.println("Comparing "+arr[i]+" and "+arr[j]);
+        if(arr[i]<arr[j]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void display(int arr[]){
+        for(int i:arr){
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+}
